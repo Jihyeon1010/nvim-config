7445c737e5abd9e2448e79dfadf640d228246ca1
Jihyeon Park <pjh06101081@outlook.com>
2025-06-30 20:11:12 +0900

    treesitter and heirline config

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,209 +1,178 @@
+-- ~/.config/nvim/lua/plugins/statusline.lua
 return {
-  "rebelot/heirline.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "folke/tokyonight.nvim",
-  },
-  event = "VeryLazy",  -- Lazy load on startup delay
-  config = function()
-    local heirline = require("heirline")
-    local conditions = require("heirline.conditions")
-    local utils = require("heirline.utils")
-    local icons = require("nvim-web-devicons")
+  {
+    "rebelot/heirline.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local heirline = require("heirline")
+      local conditions = require("heirline.conditions")
+      local utils = require("heirline.utils")
+      
+      local colors = require("tokyonight.colors").setup()
 
-    local colors = require("tokyonight.colors").setup()
-    -- set the colours of the mode of the NeoVim
-    local mode_colors = {
-      n = colors.blue,
-      i = colors.green,
-      v = colors.magenta,
-      V = colors.magenta,
-      ["\22"] = colors.magenta,
-      c = colors.yellow,
-      s = colors.orange,
-      S = colors.orange,
-      ["\19"] = colors.orange,
-      R = colors.red,
-      r = colors.red,
-      ["!"] = colors.red,
-      t = colors.red,
-    }
-
-    local get_mode_color = function()
-      local mode = vim.fn.mode():sub(1, 1) -- get short mode (e.g. 'n', 'i', etc.)
-      return mode_colors[mode] or colors.blue
-    end
-
-    -- Vimode indicatior
-    local ViMode = {
-      init = function(self)
-        self.mode = vim.fn.mode(1)
-      end,
-      static = {
-        mode_names = {
-          n = " NORMAL", i = " INSERT", v = " VISUAL", V = " V-LINE", ["\22"] = "V-BLOCK",
-          c = "󰘳  COMMAND", s = "SELECT", S = "S-LINE", ["\19"] = "S-BLOCK",
-          R = "REPLACE", r = "REPLACE", ["!"] = " SHELL", t = " TERMINAL",
+      --local colors = {
+        --bright_bg = utils.get_highlight("Folded").bg,
+        --bright_fg = utils.get_highlight("Folded").fg,
+        --red = utils.get_highlight("DiagnosticError").fg,
+        --dark_red = utils.get_highlight("DiffDelete").bg,
+        --green = utils.get_highlight("String").fg,
+        --blue = utils.get_highlight("Function").fg,
+        --gray = utils.get_highlight("NonText").fg,
+        --orange = utils.get_highlight("Constant").fg,
+        --purple = utils.get_highlight("Statement").fg,
+        --cyan = utils.get_highlight("Special").fg,
+        --diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+        --diag_error = utils.get_highlight("DiagnosticError").fg,
+        --diag_hint = utils.get_highlight("DiagnosticHint").fg,
+        --diag_info = utils.get_highlight("DiagnosticInfo").fg,
+        --git_del = utils.get_highlight("diffDeleted").fg,
+        --git_add = utils.get_highlight("diffAdded").fg,
+        --git_change = utils.get_highlight("diffChanged").fg,
+      --}
+      
+      local ViMode = {
+        init = function(self)
+          self.mode = vim.fn.mode(1)
+        end,
+        static = {
+          mode_names = {
+            n = "NORMAL",
+            no = "NORMAL",
+            nov = "NORMAL",
+            noV = "NORMAL",
+            ["no\22"] = "NORMAL",
+            niI = "NORMAL",
+            niR = "NORMAL",
+            niV = "NORMAL",
+            nt = "NORMAL",
+            v = "VISUAL",
+            vs = "VISUAL",
+            V = "V-LINE",
+            Vs = "V-LINE",
+            ["\22"] = "V-BLOCK",
+            ["\22s"] = "V-BLOCK",
+            s = "SELECT",
+            S = "S-LINE",
+            ["\19"] = "S-BLOCK",
+            i = "INSERT",
+            ic = "INSERT",
+            ix = "INSERT",
+            R = "REPLACE",
+            Rc = "REPLACE",
+            Rx = "REPLACE",
+            Rv = "REPLACE",
+            Rvc = "REPLACE",
+            Rvx = "REPLACE",
+            c = "COMMAND",
+            cv = "Ex",
+            r = "...",
+            rm = "M",
+            ["r?"] = "?",
+            ["!"] = "!",
+            t = "TERM",
+          },
+          mode_colors = {
+            n = "red",
+            i = "green",
+            v = "cyan",
+            V = "cyan",
+            ["\22"] = "cyan",
+            c = "orange",
+            s = "purple",
+            S = "purple",
+            ["\19"] = "purple",
+            R = "orange",
+            r = "orange",
+            ["!"] = "red",
+            t = "red",
+          }
         },
-      },
-      provider = function(self)
-        return " " .. self.mode_names[self.mode] .. " "
-      end,
-      hl = function(self)
-        return { fg = colors.bg, bg = mode_colors[self.mode:sub(1, 1)], bold = true }
-      end,
-      update = {
-        "ModeChanged",
-        pattern = "*:*",
-        callback = vim.schedule_wrap(function()
-          vim.cmd("redrawstatus")
-        end),
-      },
-    }
-
-    local Git = {
-      condition = conditions.is_git_repo,
-      init = function(self)
-        self.status_dict = vim.b.gitsigns_status_dict
-      end,
-      provider = function(self)
-        return " " .. self.status_dict.head .. " "
-      end,
-      hl = { fg = colors.purple },
-    }
-
-    local Diagnostics = {
-      condition = conditions.has_diagnostics,
-      static = {
-        error_icon = " ", warn_icon = " ", info_icon = " ", hint_icon = " ",
-      },
-      update = { "DiagnosticChanged", "BufEnter" },
-      provider = function()
-        --local icons = Diagnostics.static
-        local d = vim.diagnostic.count(0)
-        local output = ""
-
-        output = output .. icons.error_icon .. (d[vim.diagnostic.severity.ERROR] or 0) .. " "
-        output = output .. icons.warn_icon .. (d[vim.diagnostic.severity.WARN] or 0) .. " "
-        output = output .. icons.info_icon .. (d[vim.diagnostic.severity.INFO] or 0) .. " "
-        output = output .. icons.hint_icon .. (d[vim.diagnostic.severity.HINT] or 0)
-
-        return output
-
-      end,
-      hl = { fg = require("tokyonight.colors").setup().fg },
-    }
-
-    local FileNameBlock = {
-      init = function(self)
-        self.filename = vim.api.nvim_buf_get_name(0)
-      end,
-    }
-
-    local FileIcon = {
-      init = function(self)
-        local fname = vim.fn.expand("%:t")
-        local ext = vim.fn.expand("%:e")
-        self.icon, self.icon_color = icons.get_icon_color(fname, ext, { default = true })
-      end,
-      provider = function(self)
-        return self.icon and (self.icon .. " ")
-      end,
-      hl = function(self)
-        return { fg = self.icon_color }
-      end,
-    }
-
-    local FileName = {
-      provider = function(self)
-        local name = vim.fn.fnamemodify(self.filename, ":~:.")
-        return name == "" and "[No Name]" or name
-      end,
-      hl = { fg = colors.fg },
-    }
-
-    local FileFlags = {
-      {
-        condition = function() return vim.bo.modified end,
-        provider = "[+]",
-        hl = { fg = colors.green },
-      },
-      {
-        condition = function() return not vim.bo.modifiable or vim.bo.readonly end,
-        provider = " ",
-        hl = { fg = colors.red },
-      },
-    }
-
-    local FileNameModifier = {
-      hl = function()
-        if vim.bo.modified then
-          return { fg = "cyan", bold = true, force = true }
-        end
-      end,
-    }
-
-    FileNameBlock = utils.insert(FileNameBlock,
-      FileIcon,
-      utils.insert(FileNameModifier, FileName),
-      FileFlags,
-      { provider = '%<'}
-    )
-
-    local LSPActive = {
-      condition = conditions.lsp_attached,
-      update = { "LspAttach", "LspDetach" },
-      provider = function()
-        local names = {}
-        for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
-          table.insert(names, client.name)
-        end
-        return "  " .. table.concat(names, ", ")
-      end,
-      hl = { fg = colors.blue },
-    }
-
-    local FileType = {
-      provider = function()
-        return vim.bo.filetype ~= "" and " " .. vim.bo.filetype or ""
-      end,
-      hl = { fg = colors.orange },
-    }
-
-    local Ruler = {
-      provider = " %l:%c ",
-      hl = { fg = colors.fg },
-    }
-
-    local ScrollBar = {
-      static = {
-        sbar = { "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" },
-      },
-      provider = function(self)
-        local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-        local total = vim.api.nvim_buf_line_count(0)
-        local i = math.floor((curr_line / total) * (#self.sbar))
-        return self.sbar[math.min(i + 1, #self.sbar)]
-      end,
-      hl = { fg = colors.blue },
-    }
-
-    heirline.setup({
-      statusline = {
-        ViMode,
-        utils.surround({ "", " " }, get_mode_color, {
-          hl = { fg = colors.bg },
-        }),
-        FileNameBlock,
-        Git,
-        Diagnostics,
-        { provider = "%=" },
-        LSPActive,
-        FileType,
-        Ruler,
-        ScrollBar,
-      },
-    })
-  end,
+        provider = function(self)
+          return " %2(" .. self.mode_names[self.mode] .. "%) "
+        end,
+        hl = function(self)
+          local mode = self.mode:sub(1, 1)
+          return { fg = self.mode_colors[mode], bold = true }
+        end,
+        update = {
+          "ModeChanged",
+          pattern = "*:*",
+          callback = vim.schedule_wrap(function()
+            vim.cmd("redrawstatus")
+          end),
+        },
+      }
+      
+      local FileNameBlock = {
+        init = function(self)
+          self.filename = vim.api.nvim_buf_get_name(0)
+        end,
+      }
+      
+      local FileName = {
+        provider = function(self)
+          local filename = vim.fn.fnamemodify(self.filename, ":.")
+          if filename == "" then return "[No Name]" end
+          if #filename > 30 then
+            filename = vim.fn.pathshorten(filename)
+          end
+          return filename
+        end,
+        hl = { fg = utils.get_highlight("Directory").fg },
+      }
+      
+      local FileFlags = {
+        {
+          condition = function()
+            return vim.bo.modified
+          end,
+          provider = " [+]",
+          hl = { fg = "green" },
+        },
+        {
+          condition = function()
+            return not vim.bo.modifiable or vim.bo.readonly
+          end,
+          provider = " ",
+          hl = { fg = "orange" },
+        },
+      }
+      
+      FileNameBlock = utils.insert(FileNameBlock,
+        FileName,
+        FileFlags,
+        { provider = "%< " }
+      )
+      
+      local Ruler = {
+        provider = "%7(%l/%3L%):%2c %P",
+        hl = { fg = "blue", bold = true },
+      }
+      
+      local LSPActive = {
+        condition = conditions.lsp_attached,
+        update = { "LspAttach", "LspDetach" },
+        provider = function()
+          local names = {}
+          for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+            table.insert(names, server.name)
+          end
+          return " [" .. table.concat(names, " ") .. "]"
+        end,
+        hl = { fg = "green", bold = true },
+      }
+      
+      local DefaultStatusline = {
+        ViMode, FileNameBlock, { provider = "%=" }, LSPActive, Ruler
+      }
+      
+      heirline.setup({
+        statusline = DefaultStatusline,
+        opts = {
+          colors = colors,
+        },
+      })
+    end,
+  },
 }
-
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash", "c", "html", "javascript", "json", "lua", "luadoc",
+          "markdown", "python", "query", "regex", "tsx", "typescript",
+          "vim", "vimdoc", "yaml",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = "<C-s>",
+            node_decremental = "<C-backspace>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
