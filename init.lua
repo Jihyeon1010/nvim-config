ad85a95830cdd8b720ec092293c60a39076ae1b7
Jihyeon Park <pjh06101081@outlook.com>
2025-07-17 14:35:44 +0900

    adding about the Vscode Neovim integration

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
+-- init.lua
+if vim.g.vscode then
+  -- VSCode Neovim-specific config
+  vim.keymap.set("n", "<C-j>", "5j")
+  vim.keymap.set("n", "<C-k>", "5k")
+  -- Avoid conflicting with VSCode's UI
+else
+  -- Normal Neovim config
+end
+
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
