d1b70b0879a7fd78ca487155606dac7e1051c581
Jihyeon Park <pjh06101081@outlook.com>
2025-07-18 02:00:43 +0900

    change init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,14 @@ if vim.g.vscode then
   -- VSCode Neovim-specific config
   vim.keymap.set("n", "<C-j>", "5j")
   vim.keymap.set("n", "<C-k>", "5k")
-  -- Avoid conflicting with VSCode's UI
+  -- Avoid conflicting with VSCode'set
+
+  vim.opt.cmdheight = 0
+  vim.opt.shortmess:append("c")
+  vim.opt.showmode = false
+  vim.opt.showcmd = false
+  vim.opt.ruler = false
+  vim.opt.laststatus = 0
 else
   -- Normal Neovim config
 end
