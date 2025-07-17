a8d09966dbcca55b296dd0254b2d1b0a28f6e329
Jihyeon Park <pjh06101081@outlook.com>
2025-07-18 01:48:35 +0900

    edited smartsplit config and delete tmux

--- lua/plugins/smartsplits.lua
+++ lua/plugins/smartsplits.lua
@@ -1,8 +1,5 @@
 return {
   'mrjones2014/smart-splits.nvim',
-  'mrjones2014/legendary.nvim',
-  priority = 10000,
-  lazy = false,
 
   config = function()
     require('smart-splits').setup({
@@ -14,7 +11,7 @@ return {
 
       ignored_filetypes = {
         'NvimTree',
-        'NeoTree',
+        'neo-tree',
       },
 
       default_amount = 3,
@@ -33,20 +30,19 @@ return {
       kitty_password = nil,
       zellij_move_focus_or_tab = false,
       log_level = 'info',
-    })
+      keys = {
+        -- Movement: Control + hjkl
+        { "<C-h>", function() require("smart-splits").move_cursor_left() end,  desc = "Move to left split" },
+        { "<C-j>", function() require("smart-splits").move_cursor_down() end,  desc = "Move to below split" },
+        { "<C-k>", function() require("smart-splits").move_cursor_up() end,    desc = "Move to above split" },
+        { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
 
-    require('legendary').setup({
-      extensions = {
-        smart_splits = {
-          directions = { 'h', 'j', 'k', 'l' },
-          prev_win = '<C-\\>',
-          mods = {
-            move = '<C>',
-            resize = '<M>',
-            swap = false,
-          },
-        },
-      }
+        -- Resize: Option (Alt) + hjkl
+        { "<M-h>", function() require("smart-splits").resize_left() end,       desc = "Resize split left" },
+        { "<M-j>", function() require("smart-splits").resize_down() end,       desc = "Resize split down" },
+        { "<M-k>", function() require("smart-splits").resize_up() end,         desc = "Resize split up" },
+        { "<M-l>", function() require("smart-splits").resize_right() end,      desc = "Resize split right" },
+      },
     })
   end,
 }
